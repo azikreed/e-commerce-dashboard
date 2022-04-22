e506908b384941f24326669f0e3df9eb914f2ae7
azikreed <azikreed@gmail.com>
2022-04-22 15:07:57 +0500

    first commit

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Ali Abdullayev',
+  email: 'azikreed@gmail.com',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+};
+
+export default account;
